9423117d88e96cb35f6d6fbf819a9591d140fdb2
aclarry <admclarry@gmail.com>
2020-08-19 03:11:16 -0400

    Create basic webpage

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,30 @@
+import re
+
 from flask import Flask
+from flask import render_template
+from flask import request, jsonify
 
 
 def create_app():
     app = Flask(__name__)
 
     @app.route("/")
+    @app.route("/index")
     def main():
-        return "This is an app!"
+        return render_template("index.html")
+
+
+    @app.route("/api/_recommend-games")
+    def recommend_games():
+        search_game = request.args.get("game", "", type=str)
+        platform = request.args.get("platform", "", type=str)
+        genres_str = request.args.get("genres", "", type=str)
+        genres = genres_str.split(",")
+
+        
+
+        results = ["Result 1", "Result 2", "Result 3"]
+        return jsonify(result=results)
 
     return app
 
---
+++ app/static/js/app.js
@@ -0,0 +1,29 @@
+
+$(document).ready( function() {
+
+    $("button.submit").click(function(event) {
+        var platform = $("select#platform").val();
+        var genres = $("select#genre").val();
+        var game = $("input#game").val();
+
+        var request = {game: game, genres: genres.join(","), platform: platform};
+        var results_ele = $(".app .results");
+        console.log(request);
+        $.getJSON("/api/_recommend-games", request, function(data) {
+            results_ele.empty();
+            results_ele.css("display", "block");
+            data.result.forEach(function (item, index) {
+                // var result_ele = document.createElement("div");
+                // result_ele.text(item);
+                // results_ele.append(result_ele)
+
+                var res = results_ele.append("<p>", item);
+                // res.text(item);
+
+            });
+        });
+
+
+    });
+
+});
